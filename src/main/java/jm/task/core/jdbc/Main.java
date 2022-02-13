3a1f449c57545be4d9088c517afce6843e3ec643
akatriM <aka_trim@mail.ru>
2022-02-13 10:43:05 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
+
 package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService service = new UserServiceImpl();
+
+        service.createUsersTable();
+
+        service.saveUser("Ivan", "Ivanov", (byte) 20);
+        service.saveUser("Petr", "Petrov", (byte) 21);
+        service.saveUser("Sidr", "Sidorov", (byte) 22);
+        service.saveUser("Viktor", "Viktorov", (byte) 23);
+
+        System.out.println(service.getAllUsers());
+
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
